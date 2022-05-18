0324879ec6363a3ad7ffb8185ce96bf7e44c2c0f
Nick <nick.azurin@gmail.com>
2022-05-17 21:03:53 -0700

    delete and create routes created

--- app/controllers/api/v1/albums_controller.rb
+++ app/controllers/api/v1/albums_controller.rb
@@ -1,16 +1,39 @@
 
 class Api::V1::AlbumsController < ApplicationController
-     
+
+    #lets these keys go through
+    ALLOWED_DATA = %[title artist rating genre].freeze     
     #Defines the index action  
     def index
-        @albums = Album.all
-        render json: @albums
+        albums = Album.all
+        render json: albums
     end
 
     def show
         album = Album.find(params[:id])
         render json: album
     end
+
+    def create
+        #This line of code will iterate through payload and check keys against allowed data
+        data = json_payload.select {|k| ALLOWED_DATA.include?(k)}
+        album = Album.new(data)
+
+        if album.save
+            render json: album
+        else
+            render json: {"error": "Album not created"}
+        end
+    end
+
+    def destroy
+        album = Album.find(params[:id])
+        
+        if album.destroy
+            render json: {"Success": "Album with id " + params[:id] + " was deleted!"}
+        end
+    end
+
 end
 
 
